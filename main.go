66c4f11b5fb31b75e1e15a4406913d2df70150db
Krish <krish22092003@gmail.com>
2024-09-30 15:48:46 +0530

    successful mongoDB atlas connection

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Todo struct {
+	ID        int    `json:"_id bson:"_id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
+var collection *mongo.Collection
+
+func main(){
+	fmt.Println("API without DB")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading", err)
+	}
+
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB Atlas")
+}
